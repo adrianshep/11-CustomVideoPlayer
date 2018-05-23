a3e660f47266d64cd08385f7afce8bb8c75cc82b
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-05-23 13:01:08 -0400

    added handleRangeUpdate, handleProgress

--- scripts.js
+++ scripts.js
@@ -27,7 +27,18 @@ function updateButton() {
 
 function skip() {
   console.log(this.dataset.skip);
-  video.currentTime += this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  // console.log(this.name);
+  // console.log(this.value);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 /* hook up the event listeners */
@@ -37,3 +48,5 @@ video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
